b9234f8106c6b91b458e92ea5ba6c99f88e6f810
yabluninn <yablunin.artem@gmail.com>
2025-03-25 23:10:45 +0200

    implement header layout

--- resume-builder/src/components/layout/Header.jsx
+++ resume-builder/src/components/layout/Header.jsx
@@ -3,7 +3,10 @@ import "../../styles/Header.css";
 export default function Header() {
   return (
     <div className="header">
-      <></>
+      <div className="logo">
+        <img src="./icon.png" alt="" />
+        <p className="logo-p">Resume Builder</p>
+      </div>
     </div>
   );
 }
