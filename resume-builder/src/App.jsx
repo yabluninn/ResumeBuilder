3343ebd012ce67fbf86c259fe4e3cd7b76d862d7
yabluninn <yablunin.artem@gmail.com>
2025-03-25 17:48:18 +0200

    first commit

---
+++ resume-builder/src/App.jsx
@@ -0,0 +1,12 @@
+import "../src/styles/App.css";
+import Header from "./components/layout/Header";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
